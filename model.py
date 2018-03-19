0d367843b513fb1174a7d9625a5a3c1c1030f9db
Admin <you@example.com>
2018-03-18 22:17:31 -0500

    usded multiple cameras images

--- model.py
+++ model.py
@@ -11,17 +11,30 @@ with open('../simulator_data/driving_log.csv') as csvfile:
 imgs = []
 measurements = []
 for line in lines:
-	source_path = line[0]
-	filename = source_path.split('\\')[-1]
-	current_path = '../simulator_data/IMG/' + filename
-	image = cv2.imread(current_path)
-	imgs.append(image)
-	measurement = float(line[3])
-	measurements.append(measurement)
+	for i in range(3):
+		source_path = line[i]
+		filename = source_path.split('\\')[-1]
+		current_path = '../simulator_data/IMG/' + filename
+		image = cv2.imread(current_path)
+		measurement = float(line[3])
+		flipped_image = np.fliplr(image)
+		correction = 0.2
+		if i == 0:
+			pass
+		elif i == 1:
+				measurement += correction
+		else:
+				measurement -= correction
+		imgs.append(image)
+		measurements.append(measurement)
+		imgs.append(flipped_image)
+		measurements.append(measurement*(-1.0))
+
+
 
 X_train = np.array(imgs)
 y_train = np.array(measurements)
-print(X_train.shape, y_train[1])
+print(X_train.shape, y_train)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Lambda, Dense, Convolution2D
@@ -32,7 +45,7 @@ model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape = in_size))
 model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Convolution2D(16,5,5, activation='relu'))
+model.add(Convolution2D(6,5,5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
@@ -42,3 +55,4 @@ model.compile(loss = 'mse', optimizer = 'adam')
 model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 5)
 model.save('model.h5')
 exit()
+
